3c52aa6f8157804289bbacaee207cd789ed0cabe
daniel <potdaniel@gmail.com>
2024-05-05 01:39:40 +0300

    Update

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
+import FormRow from "../../ui/Formrow";
 import Input from "../../ui/Input";
 
 import { useUpdateUser } from "./useUpdateUser";
